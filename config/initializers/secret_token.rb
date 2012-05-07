e0315b0843e4a72be2271f841c1ed22694269422
Anthony Guidarelli <anthony.guidarelli@gmail.com>
2012-05-07 11:12:06 -0400

    Change name to PrefPlay

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3BootstrapDeviseCancan::Application.config.secret_token = 'a9e7dbb3fd733e9f7057211cce8a07affaa9704de3a413f27ef68ffcba2ceeb60108daeb7f60d224940cce888d9113ec4826dc5dce2c0b44628117fe625c8d97'
+Prefplay::Application.config.secret_token = 'a9e7dbb3fd733e9f7057211cce8a07affaa9704de3a413f27ef68ffcba2ceeb60108daeb7f60d224940cce888d9113ec4826dc5dce2c0b44628117fe625c8d97'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails3BootstrapDeviseCancan::Application.routes.draw do
+Prefplay::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
